ebd8d415e773d00c4d873f3b06d43b5f718b5ec9
Jonathan Lee <jonathan.lee.devel@gmail.com>
2025-03-01 14:39:21 +0000

    Update FLAGSMITH_CLIENT_SDK_KEY for environment configs
    Changed the FLAGSMITH_CLIENT_SDK_KEY in both production and development environment files. This ensures consistent integration with the updated Flagsmith client configurations.

--- echonexus-ui/src/environments/environment.prod.ts
+++ echonexus-ui/src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
   SUPABASE_URL: 'https://infra.echonexus.io',
   SUPABASE_PUBLIC_KEY:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6bnRsd2hwdnRtanFta3JteG1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc3NDA3OTYsImV4cCI6MjA1MzMxNjc5Nn0.HagMOjNXg9E0Ys5hlN8fgXJQE68uiFLeaHTsGCqHHOg',
-  FLAGSMITH_CLIENT_SDK_KEY: 'XypFHhGcZPb7HRwmYDegWu',
+  FLAGSMITH_CLIENT_SDK_KEY: 'g2LggGAE3x6o35EwK9ULhb',
   FLAGSMITH_API_URL: 'https://flags.echonexus.io/api/v1/',
   PAYMENTS_SERVICE_BASE_URL: 'v1/payments',
   APPLICATION_MESSAGES_SERVICE_BASE_URL: 'v1/application-messages',
