6cfbf1a50e5e8f3df5c7695179a84826aa5ad93d
Jonathan Lee <jonathan.lee.devel@gmail.com>
2025-03-22 01:33:02 +0000

    Add dividers and explanatory text to feature switches UI
    Introduce `Divider` from PrimeNG and add descriptive text to clarify feature purposes in the UI. This improves readability and enhances user understanding of feature functionality.

--- echonexus-ui/src/app/components/lib/_project/project-features-switches/project-features-switches.component.ts
+++ echonexus-ui/src/app/components/lib/_project/project-features-switches/project-features-switches.component.ts
@@ -1,6 +1,7 @@
 import {NgIf} from '@angular/common';
 import {Component, inject, input} from '@angular/core';
 import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {Divider} from 'primeng/divider';
 import {ProgressSpinnerModule} from 'primeng/progressspinner';
 import {ToggleSwitchModule} from 'primeng/toggleswitch';
 
@@ -13,6 +14,7 @@ import {ProjectStore} from '../../../../+state/project/project.store';
     ReactiveFormsModule,
     NgIf,
     ProgressSpinnerModule,
+    Divider,
   ],
   templateUrl: './project-features-switches.component.html',
   styleUrl: './project-features-switches.component.scss',
